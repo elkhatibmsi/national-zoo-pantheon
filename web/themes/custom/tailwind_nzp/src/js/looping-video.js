991271a913f696d4c6ced0c26e1bd0f8ea6302fa
DDEV-Local User <nobody@example.com>
2023-01-25 23:11:19 +0000

    gallery config export alert merge  and changes to looping video

--- web/themes/custom/tailwind_nzp/src/js/looping-video.js
+++ web/themes/custom/tailwind_nzp/src/js/looping-video.js
@@ -5,11 +5,28 @@
         var match = url.match(regExp);
         return (match&&match[7].length==11)? match[7] : false;
     }
+
+    function vidRescale($vid) {
+        let w = $vid.parent().width(),
+          h = $vid.parent().parent().height();
+          console.log(w);
+        if (w / h > 16 / 9) {
+          $vid.attr("width", w);
+          $vid.attr("height", h + 200);
+          $vid.css({ "margin-left": "0px" });
+        } else {
+          $vid.attr("width", (h / 9) * 16);
+          $vid.attr("height", h);
+          $vid.css({ left: -($vid.outerWidth() - w) / 2 });
+        }
+      }
+
     $('document').ready(function(){
         const LoopingVideo = $('.looping-video iframe');
+        vidRescale(LoopingVideo);
         let YTid = youtube_parser($('.looping-video').attr('data-source'));
         console.log(YTid);
-        LoopingVideo.attr('src', `https://www.youtube.com/embed/${YTid}?controls=0&wmode=opaque&controls=0&loop=1&mute=1&autoplay=1&showinfo=0&enablejsapi=1&playlist=${YTid}&rel=0`);
+        LoopingVideo.attr('src', `https://www.youtube.com/embed/${YTid}?controls=0&wmode=opaque&controls=0&loop=1&mute=1&autoplay=1&showinfo=0&enablejsapi=1&playlist=${YTid}&rel=0&modestbranding=1`);
     
     });
     
